039b5614825c1c79735e82899bd5aa1d69696b79
kristiansen616 <52787766+kristiansen616@users.noreply.github.com>
2020-09-30 18:13:58 -0300

    adição de formatação de valor e inclusão de mais elementos na tabela

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,25 +1,78 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+      <q-toolbar q-my-md>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-space />
+         <q-icon name="far fa-moon" />
+      <q-toggle
+        v-model="value"
+      color="yellow"
+      @input="dark()"
+    />
+    <q-icon name="fas fa-sun" />
       </q-toolbar>
     </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Menu
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
-
 <script>
+import EssentialLink from 'components/EssentialLink.vue'
+const linksData = [
+  {
+    title: 'Produtos',
+    caption: '',
+    icon: 'home',
+    link: '/'
+  }
+]
 export default {
   name: 'MainLayout',
+  components: { EssentialLink },
   data () {
     return {
+      alert: false,
+      confirm: false,
+      prompt: false,
+      value: true,
+      address: '',
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  },
+  methods: {
+    dark (val) {
+      this.$q.dark.toggle(val)
     }
   }
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,8 +4,9 @@
 
 <template>
   <div class="q-pa-md">
+  <div class="text-h4 doc-h6 q-pb-md">Produtos</div>
+    <hr>
     <q-table
-      title="Treats"
       dense
       :data="serverdata"
       :columns="columns"
@@ -18,27 +19,55 @@
       //{ label: 'None', value: 'none' },
       ]"
     >
+    <template v-slot:header="props">
+        <q-tr :props="props">
+          <q-th
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+            class="text-bold"
+          >
+            {{ col.label }}
+          </q-th>
+        </q-tr>
+      </template>
+    <template v-slot:body-cell-id="props">
+        <q-td :props="props">
+        <div>
+             {{ props.row.id_produto }}
+        </div>
+        </q-td>
+    </template>
     <template v-slot:body-cell-nome="props">
         <q-td :props="props">
         <div>
-            {{ serverdata.nome }}
+            {{ props.row.nome }}
         </div>
         </q-td>
         </template>
         <template v-slot:body-cell-valor="props">
         <q-td  :props="props">
         <div>
-             {{'R$' + serverdata.valor }}
+             {{ formataValor(props.row.valor) }}
         </div>
         </q-td>
         </template>
+         <template v-slot:body-cell-tipo="props">
+        <q-td :props="props">
+        <div>
+             {{ props.row.id_tipo }}
+        </div>
+        </q-td>
+    </template>
+         <template v-slot:body-cell-opcoes="props">
+          <q-td :props="props">
+            <q-btn dense color="primary" icon="edit"/>
+            <span>
+            Alterar
+            </span>
+          </q-td>
+        </template>
       </q-table>
-        <listlink
-          v-for="link in listlink"
-          :key="link.title"
-          v-bind="link"
-        />
-        <img v-bind:src="img" width="1400px" height="500px" q-mr-4>
   </div>
 </template>
 
@@ -50,49 +79,28 @@
 }
 </style>
 <script>
-import listlink from 'components/linkstest.vue'
-const linksData = [
-  {
-    title: 'Vue',
-    icon: 'book',
-    link: 'https://vuejs.org'
-  },
-  {
-    title: 'Github',
-    icon: 'supervised_user_circle',
-    link: 'https://github.com/kristiansen616/painel-admin'
-  },
-  {
-    title: 'Netlify',
-    icon: 'explore',
-    link: 'https://www.netlify.com'
-  },
-  {
-    title: 'Quasar',
-    icon: 'record_voice_over',
-    link: 'https://quasar.dev'
-  }
-]
+
 export default {
   name: 'App',
-  components: { listlink },
   data () {
     return {
-      listlink: linksData,
-      img: 'https://raw.githubusercontent.com/lcr-uerj/LabMetro/master/images/universe-5033471_1920.jpg?token=AMSXUNVOK65YPWFR44BB3YS7OO2PW',
       sif: 'R$',
       separator: 'cell',
       columns: [
+        { name: 'id', align: 'center', label: 'ID', field: 'id', sortable: true, headerClasses: 'bg-primary text-white text-h5' },
         {
-          name: 'noma',
+          name: 'nome',
           required: true,
           label: 'Nome',
           align: 'left',
-          field: 'nome',
+          field: 'nom',
           format: val => `${val}`,
-          sortable: true
+          sortable: true,
+          headerClasses: 'bg-primary text-white text-h5 text-uppercase'
         },
-        { name: 'val', align: 'center', label: 'Valor', field: 'valor', sortable: true }
+        { name: 'valor', align: 'center', label: 'Valor', field: 'val', sortable: true, headerClasses: 'bg-primary text-white text-uppercase text-h5' },
+        { name: 'tipo', align: 'center', label: 'Tipo', field: 'tipo', sortable: true, headerClasses: 'bg-primary text-white text-uppercase text-h5 ' },
+        { name: 'opcoes', align: 'center', label: 'Opções', field: 'opcoes', sortable: true, headerClasses: 'bg-primary text-white text-uppercase text-h5' }
       ],
       serverdata: [
         {
@@ -108,6 +116,18 @@ export default {
           this.serverdata = response.data
           console.log(this.serverdata)
         })
+    },
+    formataValor (value) {
+      console.log(typeof value)
+      if (typeof value !== 'number') {
+      // return value comentei aqui pq o valor tá vindo como string da api, aí não ia formatar
+      }
+      const formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2
+      })
+      return formatter.format(value)
     }
   },
   mounted () {
